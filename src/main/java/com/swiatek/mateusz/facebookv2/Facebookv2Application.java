8e8d6a6c1dcc405b6859485fd0486a2512d7300d
Mateusz <mswiatek2002@gmail.com>
2024-02-27 00:36:56 +0100

    init + H2 Console

---
+++ src/main/java/com/swiatek/mateusz/facebookv2/Facebookv2Application.java
@@ -0,0 +1,13 @@
+package com.swiatek.mateusz.facebookv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Facebookv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Facebookv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/swiatek/mateusz/facebookv2/Facebookv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiatek.mateusz.facebookv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Facebookv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
